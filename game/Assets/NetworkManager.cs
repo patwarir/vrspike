408cd7f4425e912a56b1493d0168ab3c46505bd9
Kevin Hu <kevinhu@Jemmys-MacBook-Pro.local>
2021-01-16 12:59:07 -0500

    Added first multiplayer functionality

---
+++ game/Assets/NetworkManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    // Update is called once per frame
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Trying to Connect to the Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server");
+        base.OnConnectedToMaster();
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+}
