89691988f04e7e977d58c03d88ee29d01dc70d73
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-01-16 12:22:57 -0500

    better ball physics

---
+++ game/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ball;
+    [SerializeField]
+    private float respawnRate = 2.0f;
+    [SerializeField]
+    private float randomRange = 1f;
+    void Start()
+    {
+        StartCoroutine(spawner());
+    }
+
+    void createBall()
+    {
+        GameObject a = Instantiate(ball) as GameObject;
+        float x_offset = Random.Range(-randomRange, randomRange);
+        float z_offset = Random.Range(-randomRange, randomRange);
+        a.transform.position = new Vector3(transform.position.x + x_offset, transform.position.y, transform.position.z + z_offset);
+    }
+
+    IEnumerator spawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnRate);
+            createBall();
+        }
+    }
+}
