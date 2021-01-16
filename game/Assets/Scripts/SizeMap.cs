4d1f89c6147fb1aa54edb2a168bfb1b410740eb9
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-01-16 02:01:31 -0500

    modified size + added transparent ground

---
+++ game/Assets/Scripts/SizeMap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeMap : MonoBehaviour
+{
+    public Vector3 scaleSize;
+    private bool setScale = false;
+    void Update()
+    {
+        if (!setScale)
+        {
+            transform.localScale = scaleSize;
+            setScale = true;
+        }
+    }
+}
