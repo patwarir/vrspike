adce68e9f9fa06c31e5b040f974a8807d8cb9b05
Kevin Hu <kevinhu@Jemmys-MacBook-Pro.local>
2021-01-16 21:51:54 -0500

    tried to fix multiplayer

--- game/Assets/NetworkPlayer.cs
+++ game/Assets/NetworkPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkPlayer : MonoBehaviour
 {
@@ -11,10 +12,19 @@ public class NetworkPlayer : MonoBehaviour
     public Transform rightHand;
     private PhotonView photonView;
 
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+        XRRig rig = FindObjectOfType<XRRig>();
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+
     }
 
     // Update is called once per frame
@@ -26,18 +36,16 @@ public class NetworkPlayer : MonoBehaviour
             leftHand.gameObject.SetActive(false);
             head.gameObject.SetActive(false);
             
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
         }
     }
 
-    void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, Transform rigTransform)
     {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
-        target.position = position;
-        target.rotation = rotation;
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
     }
 }
