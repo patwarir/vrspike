fd1f42f31d7d9c4af4e3d224d324967ac46450c2
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-01-16 20:02:46 -0500

    game logic start

---
+++ game/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private GameObject ground;
+    private enum GameStatus
+    {
+        IN_PLAY,
+        FINISHED,
+        SERVE
+    }
+    private GameStatus CurrentGameStatus;
+    private bool player1Serve = true;
+
+    void onBallHitGround()
+    {
+
+    }
+
+    void onPlayerServed()
+    {
+        player1Serve = !player1Serve;
+    }
+    void Start()
+    {
+        CurrentGameStatus = GameStatus.SERVE;
+    }
+
+    void Update()
+    {
+        
+    }
+}
