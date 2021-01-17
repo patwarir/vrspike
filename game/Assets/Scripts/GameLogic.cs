b54310659df4ac1b711799e18baa09026d37dc96
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-01-16 20:26:57 -0500

    temp GameLogic

--- game/Assets/Scripts/GameLogic.cs
+++ game/Assets/Scripts/GameLogic.cs
@@ -14,18 +14,22 @@ public class GameLogic : MonoBehaviour
         FINISHED,
         SERVE
     }
-    private GameStatus CurrentGameStatus;
+    private GameStatus CurrentGameStatus = GameStatus.SERVE;
     private bool player1Serve = true;
-
+    private int score1 = 0, score2 = 0;
+    //Called when the ball hits the ground
     void onBallHitGround()
     {
+        CurrentGameStatus = GameStatus.FINISHED;
 
     }
 
+    //Called when a player has served
     void onPlayerServed()
     {
         player1Serve = !player1Serve;
     }
+
     void Start()
     {
         CurrentGameStatus = GameStatus.SERVE;
